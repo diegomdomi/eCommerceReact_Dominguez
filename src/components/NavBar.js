ffe8a264758f6de77dd0f65b77d1b0af7cdb87e0
Diego M. Dominguez <diegomdomi@gmail.com>
2021-10-17 15:15:05 +0200

    carpeta components y archivo Navbar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return(
+        <header>
+            <nav className=" red lighten-2">
+                <div class="nav-wrapper container">
+                <a href="#" class="brand-logo">Almacén Pastelero</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Contacto</a></li>
+                </ul>
+                </div>
+            </nav>
+        </header>
+    )
+
+    }
+    
+    export default NavBar
+    
