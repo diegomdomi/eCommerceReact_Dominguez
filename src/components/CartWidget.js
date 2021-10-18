d26419dc2b80d660c1ca543d63f04c150fcff8ba
Diego M. Dominguez <diegomdomi@gmail.com>
2021-10-18 21:13:56 +0200

    incorporé CartWidget y ItemListContainer

---
+++ src/components/CartWidget.js
@@ -0,0 +1,9 @@
+const CartWidget = ()=> {
+    return(
+        <span class="material-icons">
+            add_shopping_cart
+        </span>
+    )
+}
+
+export default CartWidget
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,7 @@
+const ItemListContainer = (greeting)=>{
+    return (
+    <h3 className="blue-text text-darken-2"> soy {greeting.text}!</h3>
+    )
+  }
+  export default  ItemListContainer 
+  
